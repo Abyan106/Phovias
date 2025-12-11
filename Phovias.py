d048f5a1c7c86fdbec0e64709caf5d5d06f8d2d2
Dza-Dzh <dzakydzhn@student.upi.edu>
2025-12-11 14:55:12 +0700

    memindahkan program vendor

--- Phovias.py
+++ Phovias.py
@@ -164,4 +164,31 @@ if __name__ == "__main__":
     main_menu()
 
 
+
+
+# =========================
+# VENDOR MENU
+# =========================
+
+def vendor_menu():
+    while True:
+        print("\n=== MENU VENDOR ===")
+        print("1. Tambah Kamera")
+        print("2. Hapus Kamera")
+        print("3. Lihat Semua Kamera")
+        print("4. Logout")
+
+        choice = input("Pilih menu: ")
+
+        if choice == "1":
+            add_camera()
+        elif choice == "2":
+            delete_camera()
+        elif choice == "3":
+            list_all_cameras()
+        elif choice == "4":
+            print("👋 Keluar dari menu vendor.\n")
+            break
+        else:
+            print("❌ Pilihan tidak valid!\n")
 # testing
